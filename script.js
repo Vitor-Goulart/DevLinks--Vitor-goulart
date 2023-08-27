f2c76768f30b2f2aeb7cbc7b2d08e9079992a4a9
Vitor Goulart <vitorgoulartofficial@gmail.com>
2023-08-27 16:46:52 -0300

    :tada:initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets//avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de joão olhando para o horizonte com sorriso bobo"
+    )
+  } else {
+    img.setAttribute("src", "./assets//avatar.png")
+    img.setAttribute("alt", "Foto de joão com oculos e sorrisso faceiro")
+  }
+}
